5c35ec9a5fc40c7e44a981b6a52cad2c8ea99d12
Shiv Bhatia <shivbhatia10@gmail.com>
2023-09-04 23:39:16 +0100

    hyperboloid lmao

---
+++ main.js
@@ -0,0 +1,55 @@
+// Scene setup
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.getElementById('scene-container').appendChild(renderer.domElement);
+
+// Position the camera
+camera.position.z = 5;
+
+// Add OrbitControls for intuitive camera control
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+// Get points on circle
+const getPointOnCircle = (theta, z) => {
+    return [Math.cos(theta), Math.sin(theta), z];
+}
+
+// Create line geometry and material
+const offset = Math.PI/2;
+const numLines = 40;
+const height = 1
+var lines = [];
+var lineMaterial = new THREE.LineBasicMaterial({color: 0x0000ff});
+for (let i = 0; i < numLines; i++) {
+    var theta = 2 * Math.PI * (i/numLines);
+    const [topx, topy, topz] = getPointOnCircle(theta, height);
+    const [bottomx, bottomy, bottomz] = getPointOnCircle(theta+offset, -height);
+
+    var lineGeometry = new THREE.BufferGeometry().setFromPoints([
+      new THREE.Vector3(topx, topy, topz),
+      new THREE.Vector3(bottomx, bottomy, bottomz),
+    ]);
+    var line = new THREE.Line(lineGeometry, lineMaterial);
+    
+    lines.push(line)
+}
+
+// Add the circle and line to the scene
+lines.forEach(line => {
+  scene.add(line);
+});
+
+// Animation function
+function animate() {
+  requestAnimationFrame(animate);
+  
+  controls.update();
+  
+  renderer.render(scene, camera);
+}
+
+// Start the animation
+animate();
